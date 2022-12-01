14e3c773a6250975841fc616eaf2cb80569e036d
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-12-01 22:02:20 +0100

    something went wrong

--- src/Perkusja.java
+++ src/Perkusja.java
@@ -1,5 +1,22 @@
 public class Perkusja {
     public static void main(String[] args) {
+        PerkusjaSprzet p = new PerkusjaSprzet();
+        p.zagrajNaBebnie();
+        p.beben = false;
+        p.zagrajNaTalerzach();
+        if (p.beben == true) {
+            p.zagrajNaBebnie();
+        }
+    }
+}
+class PerkusjaSprzet {
+    boolean talerze = true;
+    boolean beben = true;
 
+    void zagrajNaTalerzach() {
+        System.out.println("brzdek, brzrzrzdeek");
+    }
+    void zagrajNaBebnie() {
+        System.out.println("bam, bam, baaa-am-am");
     }
 }
